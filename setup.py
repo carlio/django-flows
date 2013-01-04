8d3c0d6e8999f3e8220ec27988a8082cce5d12a5
carlio <git@carlcrowder.com>
2013-01-04 21:17:15 +0100

    bumping version number after "release" of master

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ from setuptools import find_packages
 import time
 
 
-#_version = "1.0.dev%s" % int(time.time())
-_version = "1.0"
+_version = "1.1.dev%s" % int(time.time())
 _packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
     
 # common dependencies
