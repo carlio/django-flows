3aa10e3fba5125ca48d7e6c604fdbb0c5da0ce5b
carlio <git@carlcrowder.com>
2013-01-04 20:57:09 +0100

    actually including the task in the commit this time

--- flows/additional/celery/cleanup_task.py
+++ flows/additional/celery/cleanup_task.py
@@ -1,3 +1,8 @@
 
 from celery.task import task
 
+@periodic_task(run_every=crontab(minute='0', hour='3'))
+def cleanup_expired_tasks():
+    count = StateModel.objects.remove_expired_state()
+    logger = cleanup_expired_tasks.get_logger()
+    logger.info("Deleted %s expired tasks' state" % count)
