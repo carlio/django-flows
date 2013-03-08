9295c281f9a450e9dd56e6d25438198ce25a37fc
carlio <git@carlcrowder.com>
2013-03-08 13:53:23 +0100

    Setting release version to 1.0.2 to include Django 1.5 compatability bugfix to tests

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 import time
 
 
-_version = "1.1.dev%s" % int(time.time())
+_version = "1.0.2"
 _packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "example"])
     
 # common dependencies
 _install_requires = [
-            'django>=1.4',
+            'django>=1.4,<1.6',
        ]
 
 setup( name='django-flows',
