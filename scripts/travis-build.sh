c9e20d1168a96e3462d6448145640b668a1b062e
carlio <git@carlcrowder.com>
2015-12-07 15:59:36 +0800

    Removing coverage for now

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,7 +3,7 @@ python scripts/travis_skip.py
 
 if [ "$?" -eq "0" ]
 then
-    coverage run django-admin.py test --settings=flows.tests.settings flows
+    django-admin.py test --settings=flows.tests.settings flows
 else
     echo "Skipping"
 fi
